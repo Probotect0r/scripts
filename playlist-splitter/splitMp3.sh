d9d2c57c9c0b7ad727329796857bf5101dd01459
probotect0r <sagar.desai@uoit.net>
2018-05-19 01:56:02 +0000

    Added playlist splitter bash script

--- playlist-splitter/splitMp3.sh
+++ playlist-splitter/splitMp3.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/bash
+
 INFO_FILE=playlist-info.txt
+
+get_timestamp() {
+    local timestamp=$(echo $1 | grep -E -o '^[[:alnum:]:]*\s' | sed -e 's/\s$//')
+    echo "$timestamp"
+}
+
 while read line; do
     if [[ $line =~ Playlist.* ]]
     then
         echo "Processing: $line"
+    else
+        time=$(get_timestamp "$line")
+        echo "$time"
     fi
 done < $INFO_FILE 
+
