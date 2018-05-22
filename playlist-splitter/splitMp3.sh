4eccc371fbb2033ffe294e6a90c1b2550a689d33
Sagar Desai <sagar.desai@uoit.net>
2018-05-21 21:10:01 -0400

    More additions to playlist splitter script

--- playlist-splitter/splitMp3.sh
+++ playlist-splitter/splitMp3.sh
@@ -4,16 +4,22 @@ INFO_FILE=playlist-info.txt
 
 get_timestamp() {
     local timestamp=$(echo $1 | grep -E -o '^[[:alnum:]:]*\s' | sed -e 's/\s$//')
+    count=$(echo $timestamp | grep -c ':')
+    echo $count
     echo "$timestamp"
+}
+
+get_current_playlist() {
+    local playlist=$(echo $line | sed -e 's/\s/-/')
+    echo "$playlist"
 }
 
 while read line; do
     if [[ $line =~ Playlist.* ]]
     then
-        echo "Processing: $line"
-        CURRENT_PLAYLIST=$(echo $line | sed -e 's/\s/-/')
+        CURRENT_PLAYLIST=$(get_current_playlist "$line")
+        echo "Processing: $CURRENT_PLAYLIST"
     else
         time=$(get_timestamp "$line")
-        echo "$time"
     fi
 done < $INFO_FILE 
