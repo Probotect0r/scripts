83a351b87e254aea7d006f431d998ec10786995f
Sagar Desai <sagar.desai@uoit.net>
2018-05-22 22:26:05 -0400

    Converted while loop to forloop in playlist splitter script.

--- playlist-splitter/splitMp3.sh
+++ playlist-splitter/splitMp3.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/bash
 
 INFO_FILE=playlist-info.txt
+line_count=$(wc -l $INFO_FILE | awk '{print $1}')
+echo $line_count
 
 get_timestamp() {
     local timestamp=$1
+    
+    # Standardize the timestamp
     count=$(echo $timestamp | grep -o ':' | wc -l)
 
-    # Standardize the timestamp
     if [[ $count == 1 ]]
     then
         timestamp=$(echo $timestamp | sed -e 's/^/00:/')
@@ -20,18 +23,20 @@ get_current_playlist() {
 }
 
 get_title() {
-    local full_time=$(echo $1 | sed -E -e 's/^.{8}\s//' )
+    local full_title=$(echo $1 | sed -E -e 's/^.{8}\s//' )
+    echo $full_title
 }
 
-while read line; do
+for ((i=1; i<=$line_count; i++))
+do
+    line=$(sed "${i}q;d" $INFO_FILE)
     if [[ $line =~ Playlist.* ]]
     then
         CURRENT_PLAYLIST=$(get_current_playlist "$line")
         echo "Processing: $CURRENT_PLAYLIST"
     else
         time=$(get_timestamp $line)
-        echo $time
-
         title=$(get_title $line)
+        echo $title
     fi
-done < $INFO_FILE 
+done
