7e6e571b4c8416909a716209182e125c4bb3935f
probotect0r <sagar.desai@uoit.net>
2018-05-26 03:37:09 +0000

    More changes to playlist splitter

--- playlist-splitter/splitMp3.sh
+++ playlist-splitter/splitMp3.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
 INFO_FILE=playlist-info.txt
+INPUT_DIR=${1:-"/home/sagar/Music"}
 
 get_timestamp() {
     local timestamp=$1
@@ -20,9 +21,35 @@ parse_playlist_title() {
     echo "$title"
 }
 
-get_title() {
+get_full_title() {
     local full_title=$(echo $1 | sed -E -e 's/^[^ ]*\s//' )
     echo $full_title
+}
+
+get_song_title() {
+    local title=$(echo $1 | sed -E -e 's/^.*-\s//')
+    echo $title
+}
+
+get_artist_name() {
+    local artist=$(echo $1 | sed -E -e 's/\s-.*$//')
+    echo $artist
+}
+
+execute_ffmpeg() {
+    input_file="$1"
+    output_file="$2"
+    song="$3"
+    artist="$4"
+    start="$5"
+    end="$6"
+ 
+    if [[ -n $end ]]
+    then
+        ffmpeg -i "${input_file}" -acodec copy -ss $start -to $end -metadata title="$song" -metadata artist="$artist" "$output_file"
+    else
+        ffmpeg -i "${input_file}" -acodec copy -ss $start -metadata title="$song" -metadata artist="$artist" "$output_file"
+    fi
 }
 
 line_count=$(wc -l $INFO_FILE | awk '{print $1}')
@@ -34,23 +61,35 @@ do
 
     if [[ $current_line =~ Playlist.* ]]
     then
+        CURRENT_PLAYLIST_ORIG=$current_line
         CURRENT_PLAYLIST=$(parse_playlist_title "$current_line")
         echo "Processing: $CURRENT_PLAYLIST"
+        mkdir $CURRENT_PLAYLIST
     else
-        current_line_timestamp=$(get_timestamp $current_line)
-        current_line_title=$(get_title "$current_line")
+        start_time=$(get_timestamp $current_line)
+        echo $start_time
+        full_title=$(get_full_title "$current_line")
+
+        artist=$(get_artist_name "$full_title")
+        song_title=$(get_song_title "$full_title")
 
         next_line_num=$(($i + 1))
         next_line=$(sed "${next_line_num}q;d" $INFO_FILE)
+
         
+        input_file="$INPUT_DIR/$CURRENT_PLAYLIST_ORIG.mp3"
+        output_file="./$CURRENT_PLAYLIST/$full_title.mp3"
+
+        echo $input_file
+        echo $output_file
+
         if [[ $next_line =~ Playlist.* ]] || [[ -z $next_line ]]
         then
             # Execute ffmpeg with no end
-            echo "$current_line_title: $current_line_timestamp -> END"
+            execute_ffmpeg  $input_file  $song_title $artist $start_time
         else
-            next_line_timestamp=$(get_timestamp $next_line)
-            # Execute ffmpeg with 
-            echo "$current_line_title: $current_line_timestamp -> $next_line_timestamp"
+            end_time=$(get_timestamp $next_line)
+            execute_ffmpeg $input_file $output_file $song_title $artist $start_time $end_time
         fi
     fi
 done
